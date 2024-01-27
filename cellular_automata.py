d36e2ba4861ad8c54daae615f02015d83dc5d0c1
ehansson <91335074+ehansson@users.noreply.github.com>
2024-01-27 20:56:14 +0100

    Update cellular_automata.py
    Added The Game of Life. It works but is very slow.

--- cellular_automata.py
+++ cellular_automata.py
@@ -9,13 +9,17 @@ Created on Fri Jan 19 18:40:04 2024
 import tkinter as tk
 import random
 from binaryconverter import dec2binarylist
+from PIL import ImageGrab
+import tkinter.filedialog as filedialog
 
 GRID = 5
 SCREENWIDTH = GRID*200
-SCREENHEIGHT = GRID*150
+SCREENHEIGHT = GRID*120
 SCREENCOLOR = "#888888"
 ONCOLOR = "#FFFFFF"
 OFFCOLOR = "#202020"
+zoom = 1.25 #My computer has 125% screensize by default, adjust to fit your own
+            #On Windows 10 it is under Settings->Display->Scale and Layout
 
 
 class Cell:
@@ -66,6 +70,56 @@ class Row:
         self.states = next_states
         self.draw()
         
+class Plane:
+    def __init__(self, canvas, firstplane):
+        self.canvas = canvas
+        self.height = len(firstplane)
+        self.width = len(firstplane[0])
+        self.cells = [[0 for w in range(self.width)] for h in range(self.height)]
+        self.states = firstplane
+        
+        self.draw()
+        
+    def draw(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                self.cells[h][w] = (Cell(self.canvas, w, h, self.states[h][w]))
+                
+    def next_generation(self):
+        next_states = [[0 for w in range(self.width)] for h in range(self.height)]
+        
+        for h in range(self.height):
+            for w in range(self.width):
+                #UL, U, UR
+                #L,  C,  R
+                #DL, D, DR
+                UL = self.states[(h-1)%self.height][(w-1)%self.width]
+                U = self.states[(h-1)%self.height][w]
+                UR = self.states[(h-1)%self.height][(w+1)%self.width]
+                L = self.states[h][(w-1)%self.width]
+                C = self.states[h][w]
+                R = self.states[h][(w+1)%self.width]
+                DL = self.states[(h+1)%self.height][(w-1)%self.width]
+                D = self.states[(h+1)%self.height][w]
+                DR = self.states[(h+1)%self.height][(w+1)%self.width]
+                
+                neigbours = [UL, U, UR, L, R, DL, D, DR]
+                
+                #Rules for Game of Life
+                if C == 0:
+                    if sum(neigbours) == 3: #If exactly 3 neigbours cell is born
+                        next_states[h][w] = 1
+                    else: 
+                        next_states[h][w] = 0
+                else: #Cell survives if it has 2 or 3 neigbours
+                    if sum(neigbours) == 3 or sum(neigbours) == 2:
+                        next_states[h][w] = 1
+                    else: #If not 2 or 3 neighbors, cell dies
+                        next_states[h][w] = 0
+                        
+        self.states = next_states
+        self.draw()
+                
 
 class CellularAutomaton1D(tk.Frame):
     def __init__(self, master = None):
@@ -84,6 +138,10 @@ class CellularAutomaton1D(tk.Frame):
         self.quit_button = tk.Button(
             self, text="Quit", command=self.master.destroy)
         self.quit_button.grid(column=0, row=1) #Crashes if command=self.quit
+        # Create "Save" button
+        self.save_button = tk.Button(
+            self, text="Save", command=self.save_image)
+        self.save_button.grid(column=3, row=0)
         # Create entry box for custom rule
         self.ruleEntry = tk.Entry(self)
         self.ruleEntry.grid(column=2, row=1)
@@ -100,7 +158,7 @@ class CellularAutomaton1D(tk.Frame):
         # Create canvas for displacing evolution of cells
         self.canvas = tk.Canvas(
             self, width=SCREENWIDTH, height=SCREENHEIGHT, bg=SCREENCOLOR)
-        self.canvas.grid(column=0, row=2, columnspan=3)
+        self.canvas.grid(column=0, row=2, columnspan=4)
         # Show current rule
         self.showrule = tk.Label(self, text = "Current rule:    ", compound=tk.LEFT)
         self.showrule.grid(column=2, row=0)
@@ -126,18 +184,89 @@ class CellularAutomaton1D(tk.Frame):
         rules = dec2binarylist(rule)
         print(rule)
         
-        # Print 150 evolutions of cells
+        # Print 120 evolutions of cells
         cells = Row(self.canvas, startingcells, rules)
-        for _ in range(150):
+        for _ in range(120):
             cells.next_row()
             
     def run(self):
         self.get_rules()
         self.print_cells()
         self.showrule.config(text="Current rule: " + str(self.rule.get()))
+        
+    def save_image(self):
+        self.update()
+        filelocation = filedialog.asksaveasfilename(defaultextension="png")
+        x = (self.winfo_rootx() + self.canvas.winfo_x())*zoom
+        y = (self.winfo_rooty() + self.canvas.winfo_y())*zoom
+        width = self.canvas.winfo_width()*zoom
+        height = self.canvas.winfo_height()*zoom
+        print(width,height,self.winfo_class())
+        self.img = ImageGrab.grab(bbox=(x,y,x+width,y+height),
+                                  include_layered_windows=False, all_screens=False)
+        self.img.save(filelocation)
+        
+class GameOfLife(tk.Frame):
+    def __init__(self, master = None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        
+        self.create_widgets()
+        self.print_cells()
+        
+    def create_widgets(self):
+        self.rule = tk.IntVar()
+        # Create "Run" button
+        self.run_button = tk.Button(
+            self, text="Generate", command=self.generate)
+        self.run_button.grid(column=0, row=0)
+        # Create "Run" button
+        self.run_button = tk.Button(
+            self, text="Run", command=self.run)
+        self.run_button.grid(column=1, row=0)
+        # Create "Quit" button
+        self.quit_button = tk.Button(
+            self, text="Quit", command=self.master.destroy)
+        self.quit_button.grid(column=0, row=1) #Crashes if command=self.quit
+        # Create "Save" button
+        self.save_button = tk.Button(
+            self, text="Save", command=self.save_image)
+        self.save_button.grid(column=3, row=0)
+        # Create canvas for displacing evolution of cells
+        self.canvas = tk.Canvas(
+            self, width=SCREENWIDTH, height=SCREENHEIGHT, bg=SCREENCOLOR)
+        self.canvas.grid(column=0, row=2, columnspan=4)
+
+    def print_cells(self):
+        startingcells = [[random.randint(0, 1) for x in range(int(SCREENWIDTH/GRID))] 
+                         for y in range(int(SCREENHEIGHT/GRID))]
+        self.cells = Plane(self.canvas, startingcells)
+        
+    def generate(self):
+        self.cells.next_generation()
+        
+    def run(self):
+        for _ in range(10):
+            self.cells.next_generation()
+            self.canvas.update()
+        print("Ten generations done")
+        
+    def save_image(self):
+        self.update()
+        filelocation = filedialog.asksaveasfilename(defaultextension="png")
+        x = (self.winfo_rootx() + self.canvas.winfo_x())*zoom
+        y = (self.winfo_rooty() + self.canvas.winfo_y())*zoom
+        width = self.canvas.winfo_width()*zoom
+        height = self.canvas.winfo_height()*zoom
+        self.img = ImageGrab.grab(bbox=(x,y,x+width,y+height),
+                                  include_layered_windows=False, all_screens=False)
+        self.img.save(filelocation)
+        
             
-app = CellularAutomaton1D()
-app.master.title("1D Cellular Automaton")
+app = GameOfLife()
+app.master.title("Game of Life")
 app.mainloop()
+
+
 
 
